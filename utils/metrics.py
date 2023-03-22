0d524a63415c15925bb854af199c2b38ab128b9b
zhexinwu <1025839251@qq.com>
2023-03-22 21:08:53 +0100

    fixed metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,4 +1,4 @@
-# supporting data shape: (H, W) and (T, H, W)
+# supporting data shape: (H, W), (T, H, W) and (T, D, H, W)
 import numpy as np
 
 from skimage.metrics import normalized_root_mse, structural_similarity
@@ -11,15 +11,15 @@ def NRMSE(img_pred: np.ndarray, img: np.ndarray):
 
 
 def SSIM(img_pred: np.ndarray, img: np.ndarray):
-    assert img_pred.ndim in [2, 3]
+    assert img_pred.ndim in [2, 3, 4]
     img_pred = img_pred[None, ...]
     img = img[None, ...]
     metric_val = structural_similarity(img_pred, img, channel_axis=0)
-    
+
     return metric_val
 
 
 REGISTERED_METRICS = {
     "NRMSE": NRMSE,
-    "SSIM": None
+    "SSIM": SSIM
 }
